7488e2509ba8730d963dce24770e84d5707a1efe
Jerome Joshua Santhakumar <jjerome@umich.edu>
2020-05-11 01:26:45 -0400

    Final

--- Salary_prediction.R
+++ Salary_prediction.R
@@ -1,23 +1,19 @@
 ---
-title: "Salary Prediction Using R"
+  title: "Salary Prediction Using R"
 author: "Jerome Joshua"
 date: "4/25/2020"
 output:
-  pdf_document: default
   html_document: default
+pdf_document: default
 ---
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-#  PART 1 -Defining  the problem
-
-### The goal of the project is to analyze the data about the various jobs of various companies and able to predict salaries of any future jobs
-
-### Importing packages  the plot.
-
-```{r cars, message=FALSE, warning=FALSE}
+  
+  
+  #  PART 1 -Defining  the problem
+  
+  ### The goal of the project is to analyze the data about the various jobs of various companies and able to predict salaries of any future jobs
+  
+  ### Importing packages  the plot.
+  
 
 library(dplyr)
 library(e1071)
@@ -29,54 +25,46 @@ library(rattle)
 library(ggpubr)
 library(rpart)
 library(xgboost)
+library(xlsx)
 
 options(scipen=999)
-```
 # PART 2- Discover 
 
 ### Load the input train and test files  and store as dataframe
-```{r cars2, message=FALSE, warning=FALSE}
 train_features <- read_csv("C:/Users/jjosh/Desktop/train_features.csv")
 
 train_salaries <- read_csv("C:/Users/jjosh/Desktop/train_salaries.csv")
 
 test_features <- read_csv("C:/Users/jjosh/Desktop/test_features.csv")
 
-```
 ### Check for duplicate values for unique key (JobId)
-There are no duplicate records  of JobId 
-```{r cars3, message=FALSE, warning=FALSE}
+### There are no duplicate records  of JobId 
 
 
 chk_duplicate<-train_features %>% group_by(jobId) %>% filter(n()>1)
 chk_duplciate2<-train_features %>% group_by(jobId)  %>% summarize(n=n())
-```
+
 ### Exploratory data analysis
 
-Below is the summary of the train_feature dataset
-Checking for how the data is structured , years of experience ranges from 0 to 24 years 
-Miles from metro city ranges from 0 to 99 miles
+# Below is the summary of the train_feature dataset
+# Checking for how the data is structured , years of experience ranges from 0 to 24 years 
+# Miles from metro city ranges from 0 to 99 miles
 
-```{r pressure}
 summary(train_features)
-```
 
-Below is the summary of the train_salaries dataset
-Salary ranges from 0 k to 301 k
+# Below is the summary of the train_salaries dataset
+# Salary ranges from 0 k to 301 k
 
-```{r pressure2}
 summary(train_salaries)
-```
 
 ### Removing outliers
-Salary cannot be zero hence removing records where salary is 0k
-```{r pressure3}
-train_salaries<-train_salaries[train_salaries$salary != 0, ]
-```
+# Salary cannot be zero hence removing records where salary is 0k
+# 
+# train_salaries<-train_salaries[train_salaries$salary != 0, ]
 
 
 ### Converting the character type format varaibles into categorical variables
-```{r pressure4}
+
 train_features$jobType<-as.factor(train_features$jobType)
 train_features$degree<-as.factor(train_features$degree)
 train_features$major<-as.factor(train_features$major)
@@ -87,12 +75,11 @@ test_features$jobType<-as.factor(test_features$jobType)
 test_features$degree<-as.factor(test_features$degree)
 test_features$major<-as.factor(test_features$major)
 test_features$industry<-as.factor(test_features$industry)
-```
 
 ### Analysis of target variable 
-Histogram of the  target variable - Salary
-Target variable follows the normal distribution 
-```{r pressure5}
+# Histogram of the  target variable - Salary
+# Target variable follows the normal distribution 
+
 hist(train_salaries$salary,
      main="Frequency analysis of target variable-Salary",
      xlab="Salary",
@@ -101,11 +88,11 @@ hist(train_salaries$salary,
      col="orange",
      freq=FALSE
 )
-```
+
 
 ### Analysis of 'years of experience' variable 
-No outliers in the years of experience
-```{r pressure6}
+# No outliers in the years of experience
+
 boxplot(train_features$yearsExperience,
         main = "Box plot analysis of 'Years of Experience'",
         xlab = "No.of years",
@@ -117,19 +104,17 @@ boxplot(train_features$yearsExperience,
 ```
 
 ### Analysis of 'miles from metroplois' variable 
-No outliers in the  distance miles of metropolis variable.
-```{r pressure7}
+# No outliers in the  distance miles of metropolis variable.
+
 boxplot(train_features$milesFromMetropolis,
         main = "Box plot analysis of 'Miles from metropolis",
         ylab = "No.of years",
         col = "orange",
         border = "brown",
         notch = TRUE)
-```
 
 ### Frequency analysis of total jobs by Major
-'None' category holds more than 50% of the total jobs
-```{r pressure8}
+# 'None' category holds more than 50% of the total jobs
 table1<-table(train_features$major)
 par(oma=c(1,2,1,1))
 par(mar=c(4,5,2,1))
@@ -139,11 +124,9 @@ barplot(table1[order(table1)],
         col=c("beige","blanchedalmond","bisque1","bisque2","bisque3"),
         main = "Count of jobs by major wise",  xlab = "Total jobs"
 )
-```
 
 ### Frequency analysis of total jobs by Industry
-The total jobs are distributed almost equally among industries.
-```{r pressure9}
+# The total jobs are distributed almost equally among industries.
 table2<-table(train_features$industry)
 par(oma=c(1,3,1,1))
 par(mar=c(4,5,2,1))
@@ -153,11 +136,9 @@ barplot(table2[order(table2)],
         col=c("beige","blanchedalmond","bisque1","bisque2","bisque3"),
         main = "Count of jobs by industry wise",  xlab = "Total jobs"
 )
-```
 
 ### Frequency analysis of total jobs by degree
-High school and None categories have more jobs than other degrees.
-```{r pressur}
+# High school and None categories have more jobs than other degrees.
 table3<-table(train_features$degree)
 par(oma=c(1,2,1,1))
 par(mar=c(4,5,2,1))
@@ -167,30 +148,22 @@ barplot(table3[order(table3)],
         col=c("beige","blanchedalmond","bisque1","bisque2","bisque3"),
         main = "Count of jobs by degree wise",  xlab = "Total jobs"
 )
-```
 
 ### Merging dependent and independent variables
-```{r pressur2, message=FALSE, warning=FALSE}
 train_all<-merge(train_features,train_salaries)
-```
 
 ### Correlation analysis of continous variables
-Correlation  between experience and salary is only 37% which is weakly correlated.
-```{r pressur3}
+# Correlation  between experience and salary is only 37% which is weakly correlated.
 cor.test(train_all$yearsExperience, train_all$salary, 
          method = "pearson")
-```
 
-Correlation  between Miles from metropolis  and salary is only  negative 29% which is weakly correlated.
-```{r pressur4}
+# Correlation  between Miles from metropolis  and salary is only  negative 29% which is weakly correlated.
 cor.test(train_all$milesFromMetropolis, train_all$salary, 
          method = "pearson")
-```
 
 
-Correlation matirx for the continous variables 
-There is no strong relation of continous variables with target variable
-```{r pressur5, warning=FALSE}
+# Correlation matirx for the continous variables 
+# There is no strong relation of continous variables with target variable
 M<-train_all[,c(7,8,9)]
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -204,14 +177,12 @@ M<-as.matrix(M)
 library(corrplot)
 Mat <- cor(M)
 corrplot(Mat, method = "circle")
-```
 
 ### Correlation analysis of categorical variables
-Since correlation cannot be computed directly , we use Analysis of variance method and finding the relation between 
-target and caterogical variables.
-
-Relation between major and salary
-```{r pre}
+# Since correlation cannot be computed directly , we use Analysis of variance method and finding the relation between 
+# target and caterogical variables.
+# 
+# Relation between major and salary
 boxplot(train_all$salary~train_all$major,
         main = "Box plot analysis of 'Salary vs Major'",
         xlab = "Major",
@@ -222,13 +193,12 @@ boxplot(train_all$salary~train_all$major,
 
 aov1<-aov(train_all$salary~train_all$major)
 summary(aov1)
-```
-p-value is very less and significant.
-There is no significant difference between the categories of major with respect to salary
+
+# p-value is very less and significant.
+# There is no significant difference between the categories of major with respect to salary
 
 
-Relation between degree  and salary
-```{r preee}
+# Relation between degree  and salary
 boxplot(train_all$salary~train_all$degree,
         main = "Box plot analysis of 'Salary vs Degree'",
         xlab = "Degree",
@@ -239,12 +209,12 @@ boxplot(train_all$salary~train_all$degree,
 
 aov2<-aov(train_all$salary~train_all$degree)
 summary(aov2)
-```
-p-value is very less and significant.
-There is  correlation   between the categories of degree  with respect to salary
 
-Relation between industry  and salary
-```{r prgjdkeee}
+# p-value is very less and significant.
+# There is  correlation   between the categories of degree  with respect to salary
+# 
+# Relation between industry  and salary
+
 boxplot(train_all$salary~train_all$industry,
         main = "Box plot analysis of 'Salary vs Industry'",
         xlab = "Industry",
@@ -255,13 +225,13 @@ boxplot(train_all$salary~train_all$industry,
 
 aov3<-aov(train_all$salary~train_all$industry)
 summary(aov3)
-```
-p-value is very less and significant.
-There is  correlation   between the categories of industry with respect to salary
+
+# p-value is very less and significant.
+# There is  correlation   between the categories of industry with respect to salary
 
 ### Baseline model
 ##### Baseline model is created by using average salary of industry 
-```` {r code1}
+
 baseline<-as.data.frame(train_all%>%group_by(industry)%>%summarise(Avg.salary=mean(salary)))
 
 train_all_with_baseline<-merge(train_all,baseline,by.x = "industry",by.y = "industry")
@@ -269,15 +239,15 @@ train_all_with_baseline<-merge(train_all,baseline,by.x = "industry",by.y = "indu
 train_all_with_baseline$Square_error<-(train_all_with_baseline$salary-train_all_with_baseline$Avg.salary)^2
 
 mean(train_all_with_baseline$Square_error)
-```
 
-Mean square error for predicted salary (baseline) is 1367.123
 
-Since the target variable is continous variable , we will use regression models to predict the salary 
+# Mean square error for predicted salary (baseline) is 1367.123
+# 
+# Since the target variable is continous variable , we will use regression models to predict the salary 
 ### Goal to build 3 models and compare the better performing models
 
 # Model 1 using Linear regression. 
-```{r model1}
+
 model1_linear_regression<-lm (formula =salary ~	degree+major+industry+yearsExperience+milesFromMetropolis+jobType , data=train_all)
 summary(model1_linear_regression)
 train_all$model1<-predict (model1_linear_regression ,train_all)
@@ -285,11 +255,12 @@ train_all$model1<-predict (model1_linear_regression ,train_all)
 train_all$Square_error<-(train_all$salary-train_all$model1)^2
 
 mean(train_all$Square_error)
-```
+
 Mean square error for predicted salary using Linear regression is  384
 
 # Model 2 using decision trees 
-```{r model2}
+
+
 model2_decisiontree=rpart(salary~degree+major+industry+yearsExperience+milesFromMetropolis+jobType,data=train_all)
 
 train_all$model2_decisiontree<-predict(model2_decisiontree ,train_all)
@@ -297,27 +268,26 @@ train_all$model2_decisiontree<-predict(model2_decisiontree ,train_all)
 train_all$Square_error2<-(train_all$salary-train_all$model2_decisiontree)^2
 
 mean(train_all$Square_error2)
-```
+
 Mean square error for predicted salary using Decision trees  is  710
 
 
 # Model 3 using xgboost
-Data has to prepared in the format of matrix  for xgboost
-COnverting categorical features into binary varaibles using one hot encoding
-``` {r matrix}
+# Data has to prepared in the format of matrix  for xgboost
+# COnverting categorical features into binary varaibles using one hot encoding
+
 category_vars = c('jobType', 'degree', 'major','industry')
 dummy_vars <- dummyVars(~ jobType +  degree + major+industry, data = train_all)
 one_hot_encode_category_vars <- as.data.frame(predict(dummy_vars, newdata = train_all))
-```
 
-Combining category features and numeric features
-```{r modelxg}
+
+# Combining category features and numeric features
+
 xg_model_train_data<-cbind(one_hot_encode_category_vars,train_all[,c(7,8,9)])
 y<-as.numeric(xg_model_train_data$salary)
-```
 
 Building XG boost Model 
-```{r xgboost}
+
 xgb <- xgboost(data = data.matrix(xg_model_train_data[,-32]), 
                label = y,
                booster = "gblinear", 
@@ -335,12 +305,10 @@ train_all$Square_error3<-(train_all$salary-train_all$xgboost_model)^2
 
 mean(train_all$Square_error3)
 
-```
 
 # Feature Engineering 
-Mean ,median ,max and min yearsExperience are computed for each of the category variables( Industry , major, degree,JobType)
+# Mean ,median ,max and min yearsExperience are computed for each of the category variables( Industry , major, degree,JobType)
 
-```{r conver, message=FALSE, warning=FALSE}
 train_all<-merge(train_features,train_salaries)
 
 jobType_stats<-train_all%>%group_by(jobType)%>%summarise(avg.salary_jobtype=mean(yearsExperience),median.salary_jobtype=median(yearsExperience),min.salary_jobtype=min(yearsExperience),max.salary_jobtype=max(yearsExperience))
@@ -364,18 +332,16 @@ names(final_data_)[1]<-"jobType"
 names(final_data_)[2]<-"degree"
 names(final_data_)[3]<-"major"
 names(final_data_)[4]<-"industry"
-```
 
-```{r modelxerg}
 dummy_vars <- dummyVars(~ jobType +  degree + major+industry, data = train_all)
 one_hot_encode_category_vars <- as.data.frame(predict(dummy_vars, newdata = final_data_))
 xg_model_train_data<-cbind(one_hot_encode_category_vars,final_data_[,c(-1,-2,-3,-4)])
 names(xg_model_train_data) <- gsub("[.]", "", names(xg_model_train_data)) 
 y<-as.numeric(xg_model_train_data$salary)
-```
+
 # Linear regression model after feature engineering
-The MSE for linear regression model after feature engineering is  384
-```{r final_model1}
+# The MSE for linear regression model after feature engineering is  384
+
 model1_linear_regression<-lm (formula =salary ~	. , data=final_data_)
 summary(model1_linear_regression)
 train_all$model1<-predict (model1_linear_regression ,final_data_)
@@ -384,12 +350,12 @@ train_all$Square_error<-(train_all$salary-train_all$model1)^2
 
 mean(train_all$Square_error)
 plot(model1_linear_regression$fitted.values,model1_linear_regression$residuals)
-```
-From the plot there exists heteroskedasticity ie) larger differences occur when response value is larger
+
+#From the plot there exists heteroskedasticity ie) larger differences occur when response value is larger
 
 # Decision tree model after feature engineering 
-The MSE for decision tree model after feature engineering is  701
-```{r finalmodel2}
+#The MSE for decision tree model after feature engineering is  701
+
 model2_decisiontree=rpart(salary~.,data=final_data_)
 
 train_all$model2_decisiontree<-predict(model2_decisiontree ,final_data_)
@@ -398,15 +364,14 @@ train_all$Square_error2<-(train_all$salary-train_all$model2_decisiontree)^2
 
 mean(train_all$Square_error2)
 fancyRpartPlot(model2_decisiontree)
-```
 
 
 # XG boost model after feature engineering
-Combining category features and numeric features
-The MSE of  xgboost  model after feature engineering is  103
+# Combining category features and numeric features
+# The MSE of  xgboost  model after feature engineering is  103
+ 
+# Building XG boost Model 
 
-Building XG boost Model 
-```{r xgboostmofefdel}
 xg_model_train_data[,c(1:48)] <-round(xg_model_train_data[,c(1:48)],0)
 xgb_final <-xgboost(data = data.matrix(xg_model_train_data[,-32]), 
                     label = y,
@@ -425,12 +390,10 @@ xg_model_train_data$Square_error3<-(xg_model_train_data$salary-xg_model_train_da
 
 mean(xg_model_train_data$Square_error3)
 
-```
 ### Plot of important variables usign XG boost model
-```{r tree}
+
 importance_matrix <- xgb.importance( model = xgb_final)
-xgb.plot.importance(importance_matrix)
-```
+xgb.plot.importance(importance_matrix,top_n = 10)
 
 
 
@@ -446,9 +409,8 @@ The best model is XG boost model.
 # PART 4 -Deployment
 
 ### Scoring the test data using the final XG boost model.
-Importing the test data and extracting features and scoring the test data using the final model
+#Importing the test data and extracting features and scoring the test data using the final model
 
-```{r automation}
 test_features <- read_csv("C:/Users/jjosh/Desktop/test_features.csv")
 
 jobType_stats<-test_features%>%group_by(jobType)%>%summarise(avg.salary_jobtype=mean(yearsExperience),median.salary_jobtype=median(yearsExperience),min.salary_jobtype=min(yearsExperience),max.salary_jobtype=max(yearsExperience))
@@ -482,5 +444,4 @@ names(test_data_scored) <- gsub("[.]", "", names(test_data_scored))
 
 
 final_data_$predicted_salary<- predict(xgb_final, newdata = as.matrix(test_data_scored))
-write.csv(final_data_,"test_scored.csv")
-```
+write.xlsx(final_data_,"test_scored.xlsx")
